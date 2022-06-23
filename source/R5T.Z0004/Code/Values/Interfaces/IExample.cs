e3500d8777ec4c4c084ece8ae3aa6050a6b5faaa
davidcoats <david@getsafetycone.com>
2022-06-23 12:12:42 -0700

    Interim changes.

--- source/R5T.Z0004/Code/Values/Interfaces/IExample.cs
+++ source/R5T.Z0004/Code/Values/Interfaces/IExample.cs
@@ -8,6 +8,7 @@ namespace R5T.Z0004
     [ValuesMarker]
     public interface IExample : IValuesMarker
     {
+        public string GuidString => "9A19103F-16F7-4668-BE54-9A1E7A4F7556";
         public string LibraryName => "R5T.Example";
         public string ProjectName => "R5T.Example";
         public string RepositoryDescription => "An example repository.";
